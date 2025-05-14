fbb6124e85fa33c05ab511a7d036ee81b6ba4dfd
FrostyAceHook <69618630+FrostyAceHook@users.noreply.github.com>
2025-05-14 11:56:01 +1000

    plotting wrapper

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,8 @@ from maths import pi
 from ranges import linspace, logspace
 from rungekutta import rk4
 
+from plot import plt, figure, plot
+
 import units as u
 
 from rational import Rational
---
+++ src/plot.py
@@ -0,0 +1,35 @@
+
+# Avoid super expensive import on start-up, only do it when used.
+plt = None
+def _init():
+    global plt, ion
+    if plt is None:
+        import matplotlib.pyplot as _plt
+        plt = _plt
+        plt.ion()
+
+
+def figure(xlabel=None, ylabel=None, title=None, grid=False):
+    _init()
+    plt.figure(figsize=(8, 5))
+    plt.grid(grid)
+    if xlabel is not None:
+        plt.xlabel(xlabel)
+    if ylabel is not None:
+        plt.xlabel(ylabel)
+    if title is not None:
+        plt.title(title)
+
+def plot(x, y, label=None, color=None, linestyle=None, linewidth=None,
+            marker=None, markersize=None, markerfacecolor=None,
+            markeredgecolor=None, alpha=None):
+    _init()
+    plt.plot(x, y, label=label, color=color, linestyle=linestyle,
+            linewidth=linewidth, marker=marker, markersize=markersize,
+            markerfacecolor=markerfacecolor, markeredgecolor=markeredgecolor,
+            alpha=alpha)
+
+    # Enable legend if anything has been labelled.
+    handles, labels = plt.gca().get_legend_handles_labels()
+    if any(lbl for lbl in labels):
+        plt.legend()
